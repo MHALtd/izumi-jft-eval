12b164a9c0e4be005f0cf66164f0f49d4331f9d9
MHALtd <hasbi1117@gmail.com>
2024-06-25 10:05:43 +0700

    tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFDA78",
+        secondary: "#FF7F3E",
+        tartiary: "#003285",
+        pale: "#2A629A",
+        brokenwhite: "#FAFAFA",
+      },
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
 };
