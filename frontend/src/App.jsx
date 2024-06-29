c8a7f0c56bff918d7d0fa918a4b0d18c204df49a
MHALtd <hasbi1117@gmail.com>
2024-06-29 17:44:08 +0700

    base index file fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import BunpouTest from "./pages/BunpouTest";
-import ChoukaiTest from "./pages/ChoukaiTest";
-import DokkaiTest from "./pages/DokkaiTest";
-import GoiTest from "./pages/GoiTest";
+import "./App.css";
+import "./index.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Test from "./components/Test";
 
 function App() {
   return (
-    <Routes>
-      <Route>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/bunpou-test" component={BunpouTest} />
-        <Route path="/choukai-test" component={ChoukaiTest} />
-        <Route path="/dokkai-test" component={DokkaiTest} />
-        <Route path="/goi-test" component={GoiTest} />
-      </Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Home />
+      <Test />
+      <Footer />
+    </>
   );
 }
 
