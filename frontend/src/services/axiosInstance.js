effa9cf12376a2a029d91e98515a2a6d166d5675
MHALtd <hasbi1117@gmail.com>
2024-07-01 03:39:44 +0700

    axios package install and get api from backend

---
+++ frontend/src/services/axiosInstance.js
@@ -0,0 +1,34 @@
+// src/services/axiosInstance.js
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5555", // Ganti PORT dengan port backend Anda
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Tambahkan interceptor untuk menangani request dan response jika diperlukan
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Lakukan sesuatu sebelum request dikirim
+    return config;
+  },
+  (error) => {
+    // Tangani error request
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // Lakukan sesuatu dengan data response
+    return response;
+  },
+  (error) => {
+    // Tangani error response
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
