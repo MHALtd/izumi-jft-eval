f9a3d4862d14fa38219d0c466937591f46bfc3a4
MHALtd <hasbi1117@gmail.com>
2024-08-23 03:25:07 +0700

    env key update

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_LINK, // Ganti PORT dengan port backend Anda
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
