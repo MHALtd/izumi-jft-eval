e5b1d098d54f720568d6f7902c730fcb6061ca5e
MHALtd <hasbi1117@gmail.com>
2024-09-08 14:01:37 +0700

    navbar change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-import logo from "../assets/logo.png";
-
 const Navbar = () => {
   return (
     <nav className="bg-primary md:px-14 p-4 max-w-screen mx-auto text-secondary fixed top-0 right-0 left-0">
       <div className="text-lg container flex mx-auto justify-between md:justify-center items-center font-medium">
         <div className="flex space-x-14 items-center">
           <a className="text-2xl font-semibold flex items-center space-x-3 text-tartiary" href="/">
-            <img src={logo} alt="" className="w-10 inline-block items-center" /> <span>IZUMI</span>
+            <p>IZUMI</p>
           </a>
         </div>
       </div>
