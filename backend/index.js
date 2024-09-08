cd553a5a8c3f6ef8a5703f279dfa6965a175685e
MHALtd <hasbi1117@gmail.com>
2024-09-08 13:51:29 +0700

    backend auth feature delete

--- backend/index.js
+++ backend/index.js
@@ -20,27 +20,6 @@ app.get("/", (request, response) => {
 
 app.use(questionsRoute);
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  UserModel.findOne({ email: email }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json("Success");
-      } else {
-        res.json("Password incorrect");
-      }
-    } else {
-      res.json("User not exist");
-    }
-  });
-});
-
-app.post("/register", (req, res) => {
-  UserModel.create(req.body)
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
 mongoose
   .connect(mongoDBURL)
   .then(() => {
